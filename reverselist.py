1f845471a390248e3a887aedd6881444bf69731e
MD HAKIKUL <thehakikul@gmail.com>
2025-08-27 21:20:01 +0530

    Update reverselist.py

--- reverselist.py
+++ reverselist.py
@@ -3,7 +3,8 @@ original_list=[1,2,3,4,5,6,7,8,9,10]
 data=original_list[0:5]
 reverse_first=data.copy()
 reverse_first.reverse()
-
+print("Original list:",orginal_list)
 print(f"Extracted first five elements:{data}")
+
 
 print("Reversed Elements:",reverse_first)
